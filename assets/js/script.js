347ba3a772f636898a0255fca8c785548b86d9a3
Christopher Meddings <c.m.meddings@gmail.com>
2023-06-02 09:19:02 +0000

    Add further style to game container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+const letterContainer = document.getElementsByClassName("letter-container");
+
+let gameQuestion = [
+    {question: "Name this golfer",
+    hangman: "jordanspieth",
+    hint1: "Studied at University of Texas",
+    hint2: "Won his first major in 2015"},
+    {question: "Golf terminology",
+    hangman: "albatross",
+    hint1: "nickname for a score",
+    hint2: "type of bird"}
+]
+
 function runGame() {
 
 }
