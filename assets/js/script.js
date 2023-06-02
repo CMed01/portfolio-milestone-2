e6ec9fa3fee914307daf6d4884113439d2a86cf3
Christopher Meddings <c.m.meddings@gmail.com>
2023-06-02 10:29:41 +0000

    Add letter buttons and style when pages loads

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-const letterContainer = document.getElementsByClassName("letter-container");
-
 let gameQuestion = [
     {question: "Name this golfer",
     hangman: "jordanspieth",
@@ -11,7 +9,22 @@ let gameQuestion = [
     hint2: "type of bird"}
 ]
 
-function runGame() {
+const letterContainer = document.getElementById("letter-container");
+
+// Initial function when page loads
+function pageLoad() {
+    // Add hangman letter to letter container
+    for (let i = 65; i < 91; i++) {
+        let button = document.createElement("button");
+        button.classList.add("letters");
+        button.innerText = String.fromCharCode(i);
+        letterContainer.append(button);
+        } 
+    }
+
+window.onload = pageLoad();
+
+function startGame() {
 
 }
 
